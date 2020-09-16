53dff53ca609ef459576fbaa9fbf50191c82a188
Rissan <ripattna@gmail.com>
2020-09-16 21:37:03 +0530

    Code Change
    Code Change

--- Python/SqlConnect/MySqlConnection.py
+++ Python/SqlConnect/MySqlConnection.py
@@ -1,13 +1,13 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
+conn = mysql.connector.connect(
     host="localhost",
     user="root",
     password="root",
     use_pure=True
 )
 
-my_cursor = mydb.cursor()
+my_cursor = conn.cursor()
 my_cursor.execute("show databases")
 
 for x in my_cursor:
