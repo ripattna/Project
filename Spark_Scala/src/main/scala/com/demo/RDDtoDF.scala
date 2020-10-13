82178abd8dec988ac73fe25e5971b0dc80ba0c84
Rissan <ripattna@gmail.com>
2020-10-13 18:31:27 +0530

    Code Change
    Code Change

--- Spark_Scala/src/main/scala/com/demo/RDDtoDF.scala
+++ Spark_Scala/src/main/scala/com/demo/RDDtoDF.scala
@@ -1,34 +1,36 @@
 package com.demo
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-import org.apache.spark.sql.types.StructType
+
 
 object RDDtoDF {
 
   def main(args: Array[String]): Unit = {
 
     // Create SparkSession
-    val spark = SparkSession.builder().appName("RDDTransformation").master("local").getOrCreate()
+    //val spark = SparkSession.builder().appName("RDDTransformation").master("local").getOrCreate()
+    val spark: SparkSession = SparkSession.builder().master("local[1]").appName("SparkByExample").getOrCreate()
 
     // Creating log level
     spark.sparkContext.setLogLevel("WARN")
 
     import spark.implicits._
-    val columns = Seq("language","users_count")
+    //val columns = Seq("language","users_count")
     val data = Seq(("Java", "20000"), ("Python", "100000"), ("Scala", "3000"))
     val rdd = spark.sparkContext.parallelize(data)
 
-    // Converting RDD to DF
+    // Converting RDD to DF (USING toDF())
     //val dfFromRDD1 = rdd.toDF
     val dfFromRDD1 = rdd.toDF("language","users_count")
-
     dfFromRDD1.printSchema()
     dfFromRDD1.show()
 
-    //val columns = Seq("language","users_count")
-    //val dfFromRDD2 = spark.createDataFrame(rdd).toDF(columns:_*)
+    // Converting RDD to DF (USING createDataFrame)
+    val columns = Seq("language","users_count")
+    val dfFromRDD2 = spark.createDataFrame(rdd).toDF(columns: _*)
+    dfFromRDD2.printSchema()
+    dfFromRDD2.show()
+
   }
 
 }
