0efd0df2cdf9ee4abbf8041991b853116884b919
ripattna <ripattna@gmail.com>
2021-08-31 19:54:26 +0530

    Code Change

--- Python/UnitTest/test_calc.py
+++ Python/UnitTest/test_calc.py
@@ -1,5 +1,5 @@
 import unittest
-import calc
+from Python.UnitTest import calc
 
 
 class TestCalc(unittest.TestCase):
@@ -13,6 +13,11 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.subtract(10, 5), 5)
         self.assertEqual(calc.subtract(-1, 1), -2)
         self.assertEqual(calc.subtract(-1, -1), 0)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(10, 5), 2)
+        self.assertEqual(calc.divide(-1, 1), -1)
+        self.assertEqual(calc.divide(-1, -1), 1)
 
     def test_multiply(self):
         self.assertEqual(calc.multiply(11, 5), 55)
