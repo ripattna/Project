03f039c8199ee97f395abf62592220401bc827fc
ripattna <ripattna@gmail.com>
2021-12-29 15:51:53 +0530

    Code Change

--- PySpark/com/dataframe/DataExtract.py
+++ PySpark/com/dataframe/DataExtract.py
@@ -28,11 +28,8 @@ def spark_execute(EXT_QRY_PATH, AUD_QRY_PATH, EXT_HDFS_PATH, ZIP_FLAG):
     try:
 
         logging.info("creating spark session")
-
         spark = (SparkSession.builder
-
                  .master("yarn")
-
                  .config(conf=SparkConf()
                          .set("spark.port.maxRetries", 25)
                          .set("spark.yarn.queue", "scheduled.etl")
@@ -50,20 +47,14 @@ def spark_execute(EXT_QRY_PATH, AUD_QRY_PATH, EXT_HDFS_PATH, ZIP_FLAG):
                          .set("spark.sql.shuffle.partitions", 200)
                          )
                  .enableHiveSupport()
-                 .getOrCreate()
-
-                 )
+                 .getOrCreate())
 
         logging.info("spark session created")
-
     # logging.info("spark session created")
 
     except Exception as e:
-
         logging.error("could not create spark session due to exception")
-
         logging.exception(e)
-
         sys.exit(1)
 
     try:
@@ -104,7 +95,6 @@ def spark_execute(EXT_QRY_PATH, AUD_QRY_PATH, EXT_HDFS_PATH, ZIP_FLAG):
         sys.exit(1)
 
     try:
-
         logging.info("running extraction spark sql")
         e_df = spark.sql(eo_query)
         logging.info("spark sql executed and dataframe got created")
@@ -118,10 +108,8 @@ def spark_execute(EXT_QRY_PATH, AUD_QRY_PATH, EXT_HDFS_PATH, ZIP_FLAG):
     if ZIP_FLAG.upper() == 'N':
 
         try:
-
             logging.info("writing data frame into csv file")
-            e_df.repartition(4).write.csv(EXT_HDFS_PATH, header=False, mode='overwrite', sep='', nullValue='\u0000',
-                                          emptyValue='\u0000')
+            e_df.repartition(4).write.csv(EXT_HDFS_PATH, header=False, mode='overwrite', sep='', nullValue='\u0000', emptyValue='\u0000')
             logging.info("data frame got written into csv files")
 
         except Exception as e:
@@ -131,28 +119,21 @@ def spark_execute(EXT_QRY_PATH, AUD_QRY_PATH, EXT_HDFS_PATH, ZIP_FLAG):
             sys.exit(1)
 
     elif ZIP_FLAG.upper() == 'Y':
-
         try:
-
             logging.info("Writing data frame to zipped csv files")
             e_df.repartition(4).write.csv(EXT_HDFS_PATH, compression="gzip", header=False, mode='overwrite', sep='',
                                           nullValue='\u0000', emptyValue='\u0000')
             logging.info("data frame got written into zipped csv files")
 
         except Exception as e:
-
             logging.error("writing dataframe into zipped csv file failed")
             logging.exception(e)
             sys.exit(1)
-
     try:
-
         logging.info("running audit spark sql")
         a_df = spark.sql(ao_query)
         logging.info("spark sql executed and dataframe got created")
-
     except Exception as e:
-
         logging.error("Audit spark sql failed due to exception")
 
     # df1 = spark.read.format("csv").option("header", "false").load(EXT_HDFS_PATH+"/*.csv")
@@ -167,9 +148,7 @@ def spark_execute(EXT_QRY_PATH, AUD_QRY_PATH, EXT_HDFS_PATH, ZIP_FLAG):
     logging.info("Source table count {0}".format(table_cnt))
 
     if int(extract_cnt) == int(table_cnt):
-
         logging.info("Audit count matched successfully")
-
     else:
 
         logging.info("Audit Count not matched")
@@ -177,17 +156,15 @@ def spark_execute(EXT_QRY_PATH, AUD_QRY_PATH, EXT_HDFS_PATH, ZIP_FLAG):
 
 # df.coalesce(1).write.format("com.databricks.spark.csv").save("/user/dw/gopal/debit_hsh")
 
-
 def main():
     try:
 
         if len(sys.argv) == 13:
-
             parser = argparse.ArgumentParser()
             parser.add_argument("CATEGORY", help="Provide category name to Pyspark SQL")
             parser.add_argument("PROCDATE", help="Provide IWDATE to Pyspark SQL")
-            ##parser.add_argument("SALT_VALUE",help="Provide Salt Value to Pyspark SQL")
-            ##parser.add_argument("SALT_DATE",help="Provide Salt Date to Pyspark SQL")
+            # parser.add_argument("SALT_VALUE",help="Provide Salt Value to Pyspark SQL")
+            # parser.add_argument("SALT_DATE",help="Provide Salt Date to Pyspark SQL")
             parser.add_argument("ACTIVE_FLAG", help="Provide active flag status for this data category")
             parser.add_argument("ZIP_FLAG", help="Provide Zip flag status for this data category")
             parser.add_argument("FREQUENCY", help="Provide extraction frequency for this data category")
@@ -199,102 +176,69 @@ def main():
             parser.add_argument("SUBS_KEY_SET",
                                 help="Provide variable names which needs to be replaced in query for this data category")
             parser.add_argument("SUBS_VAL_SET",
-                                help="Provide variable values which needs to be replaced in query for this data category")
+                                help= "Provide variable values which needs to be replaced in query for this data category")
 
             global args
 
             args = parser.parse_args()
-
             logging.info("***Below are the Arguments passed***")
-
             for attr, value in args.__dict__.items():
                 logging.info('{0}----{1}'.format(attr, value))
 
         else:
 
             logging.error("12 arguments are needed but {0} are passed".format(len(sys.argv) - 1))
-
             raise InvalidNoOfArgError("Please check no of arguments passed")
 
-
-
     except InvalidNoOfArgError as e:
-
         logging.exception(e)
-
         sys.exit(1)
 
     global PROC_DT
-
     PROC_DT = datetime.datetime.strptime(args.PROCDATE, '%Y-%m-%d')
 
     if args.ACTIVE_FLAG.upper() == "N":
-
         logging.info(
             "Active flag is set to {}. So skipping extraction for {} data category".format(args.ACTIVE_FLAG.upper(),
                                                                                            args.CATEGORY))
-
         sys.exit(0)
-
     else:
-
         logging.info("Active flag is set to Y. So continuing extraction..")
-
     if args.FREQUENCY.upper() == "DAILY":
-
         logging.info("Extraction frequency is set to {}..so continuing extraction..".format(args.FREQUENCY.upper()))
 
-
-
     elif args.FREQUENCY.upper() == "WEEKLY":
-
         logging.info("Extraction frequency is set to {}..now checking day of week ...".format(args.FREQUENCY.upper()))
-
         if args.DAY_OF_WEEK.upper() == PROC_DT.strftime('%A').upper():
-
             logging.info(
                 "{} extraction is set to run on {}..so continuing extraction..".format(args.DAY_OF_WEEK.upper()))
-
         else:
-
             logging.info(
                 "{} extraction is not set to run on {}..so skipping extraction and exiting from script.".format(
                     args.CATEGORY, PROC_DT.strftime('%A').upper()))
-
             sys.exit(0)
-
     elif args.FREQUENCY.upper() == "MONTHLY":
-
         logging.info("Extraction frequency is set to {}..now checking date of month ...".format(args.FREQUENCY.upper()))
-
         if args.DATE_OF_MONTH == PROC_DT.strftime('%d').upper():
-
             logging.info(
                 "{} extraction is set to run on {} date of month..so continuing extraction..".format(args.CATEGORY,
                                                                                                      PROC_DT.strftime(
                                                                                                          '%d').upper()))
-
         else:
-
             logging.info(
                 "{} extraction is not set to run on {} date of month..so skipping extraction and exiting from script..".format(
                     args.CATEGORY, PROC_DT.strftime('%d').upper()))
-
             sys.exit(0)
-
     else:
-
         logging.info("{} is invalid entry for frequency..exiting script with error".format(args.FREQUENCY.upper()))
-
         sys.exit(0)
-
     logging.info("calling spark_execute function")
-
     spark_execute(args.INP_EXT_QRY, args.INP_AUD_QRY, args.OP_EXT_PATH, args.ZIP_FLAG)
 
 
-logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', filename='/home/e110682/updated/dw_spark_ext.log',
-                    filemode='a', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(
+    format='%(asctime)s %(levelname)s %(message)s', filename='/home/log/dw_spark_ext.log', filemode='a',
+    level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
 if __name__ == "__main__":
     # sys.exit(main())
